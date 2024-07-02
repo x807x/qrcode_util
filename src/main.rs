6b03bcf99b6f1056e4fe0f3ecc71f7efe32237d7
ckeisc807 <s10855168@gmail.com>
2024-07-02 21:54:36 +0800

    generate qrcode image

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use eframe::egui;
+use eframe::egui::{self, TextureHandle};
+use image::{ImageBuffer, Rgba};
+use qrcode::QrCode;
+use std::option::Option;
+use integer_sqrt::IntegerSquareRoot;
 
 fn main() -> Result<(), eframe::Error> {
     env_logger::init();
@@ -16,14 +20,31 @@ fn main() -> Result<(), eframe::Error> {
 
 struct MyApp {
     name: String,
+    texture: Option<TextureHandle>,
 }
 
 impl Default for MyApp {
     fn default() -> Self {
         Self {
             name: "World".to_owned(),
+            texture: None,
         }
     }
+}
+
+fn load_qrcode_from_text(text: &str) -> Result<egui::ColorImage, image::ImageError> {
+    let code = QrCode::new(text).unwrap();
+    let image_buffer: ImageBuffer<Rgba<u8>, Vec<u8>> = code.render::<Rgba<u8>>().build();
+    image_buffer.save("image.png").unwrap();
+    let pixels = image_buffer.as_flat_samples();
+    let len = image_buffer.len().integer_sqrt()/2;
+    
+    let size: [usize; 2] = [len, len];
+    println!("{:#?}", size);
+    Ok(egui::ColorImage::from_rgba_unmultiplied(
+        size,
+        pixels.as_slice(),
+    ))
 }
 
 impl eframe::App for MyApp {
@@ -35,7 +56,14 @@ impl eframe::App for MyApp {
                 ui.text_edit_singleline(&mut self.name);
             });
             ui.label(format!("Hello, {}!", self.name));
-            ui.image(egui::include_image!("../images/image.png"));
+            let texture = self
+                .texture
+                .get_or_insert(egui::Context::default().load_texture(
+                    "QRcode",
+                    load_qrcode_from_text(self.name.as_str()).unwrap(),
+                    egui::TextureOptions::default(),
+                ));
+            ui.image(&texture.to_owned());
         });
     }
 }
